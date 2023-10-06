18bbbcc20481c1ef9f04d117437b12c31b5cd173
Apipark Withedvorrakit <apiparkwithedvorrakit@Apiparks-MacBook-Pro.local>
2023-10-06 16:29:44 +0700

    social config targer_blank

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -19,27 +19,27 @@ const Socials = () => {
   className='hidden xl:flex ml-24'>
     <ul className='flex gap-x-4'>
       <li>
-        <a href='http://www.facebook.com' target='_blank'>
+        <a href='http://www.facebook.com'>
           <ImFacebook /> 
         </a>
       </li>
       <li>
-        <a href='http://www.twitter.com' target='_blank'>
+        <a href='http://www.twitter.com'>
           <ImTwitter /> 
         </a>
       </li>
       <li>
-        <a href='http://www.pinterest.com' target='_blank'>
+        <a href='http://www.pinterest.com'>
           <ImPinterest /> 
         </a>
       </li>
       <li>
-        <a href='http://www.instagram.com' target='_blank'>
+        <a href='http://www.instagram.com'>
           <ImInstagram /> 
         </a>
       </li>
       <li>
-        <a href='http://www.youtube.com' target='_blank'>
+        <a href='http://www.youtube.com'>
           <ImYoutube /> 
         </a>
       </li>
